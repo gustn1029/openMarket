a2679acda04fd29907a023d56933cf20f9203a1f
hs0820 <gustn1029@gmail.com>
2024-08-03 16:00:05 +0900

    로그인 기능 구현

--- js/login.js
+++ js/login.js
@@ -1,7 +1,6 @@
 import "../css/login.css";
 
 const template = () => {
-
   const login = `
           <header class="pt-[100px] text-center mb-[70px]">
               <a href="/openMarket" class="inline-block">
@@ -20,9 +19,9 @@ const template = () => {
               </ul>
               <form class="customer">
                   <label for="userId" class="hidden">아이디 입력</label>
-                  <input type="text" placeholder="아이디" />
-                  <label for="userId" class="hidden">비밀번호 입력</label>
-                  <input type="password" placeholder="비밀번호" />
+                  <input type="text" placeholder="아이디" id="userId" required />
+                  <label for="userPassword" class="hidden">비밀번호 입력</label>
+                  <input type="password" placeholder="비밀번호" id="userPassword" required />
                   <button type="submit">로그인</button>
               </form>
               <div class="sign__up">
@@ -41,32 +40,78 @@ const template = () => {
 export const Login = () => {
   document.body.innerHTML = template();
 
-  let loginUser = "customer";
+  const url = "https://openmarket.weniv.co.kr";
+  let loginUser = "BUYER";
   const customerBtn = document.querySelector(".customer");
   const sellerBtn = document.querySelector(".seller");
   const form = document.querySelector(".login-wrap form");
-  console.log(form);
+  const userId = document.getElementById("userId");
+  const userPassword = document.getElementById("userPassword");
+  let errorMessage = null;
 
-  customerBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    sellerBtn.classList.remove("active");
-    !customerBtn.classList.contains("active") &&
-      customerBtn.classList.add("active");
+  const userBtnClickHandler = (clickUserInfo, unclickUserInfo, infoText) => {
+    unclickUserInfo.classList.remove("active");
+    !clickUserInfo.classList.contains("active") &&
+      clickUserInfo.classList.add("active");
     if (form.classList.contains("seller")) {
       form.classList.remove("seller");
       form.classList.add("customer");
     }
+    loginUser = infoText;
+    userId.value = "";
+    userPassword = "";
+    errorMessage !== null && errorMessage.remove();
+    errorMessage = null;
+  };
+
+  const submitHandler = async () => {
+    errorMessage !== null && errorMessage.remove();
+
+    const idVal = userId.value;
+    const passwordVal = userPassword.value;
+
+    const loginData = {
+      username: idVal,
+      password: passwordVal,
+      login_type: loginUser
+    };
+
+    const res = await fetch(`${url}/accounts/login/`, {
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(loginData)
+    });
+    const json = await res.json();
+
+    if(res.ok) {
+      const user = {
+        user_type: json.user_type,
+        token: json.token
+      }
+
+      localStorage.setItem("user", JSON.stringify(user));
+      window.location.href = "/openMarket";
+    } else {
+      userPassword.insertAdjacentHTML("afterend",`<p class="error__message">아이디 또는 비밀번호가 일치하지 않습니다.</p>`);
+
+      errorMessage = document.querySelector(".error__message");
+    };
+  }
+
+  customerBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    userBtnClickHandler(customerBtn, sellerBtn, "BUYER");
   });
+
   sellerBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    customerBtn.classList.remove("active");
-    !sellerBtn.classList.contains("active") &&
-      sellerBtn.classList.add("active");
-    if (form.classList.contains("customer")) {
-      form.classList.remove("customer");
-      form.classList.add("seller");
-    }
-
-    loginUser = "seller";
+    userBtnClickHandler(sellerBtn, customerBtn, "SELLER");
   });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitHandler();
+  })
 };
