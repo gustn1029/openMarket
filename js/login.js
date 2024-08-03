db96fff7c0ccb1eb8e0696b2cfa417bd79988375
hs0820 <gustn1029@gmail.com>
2024-08-03 20:16:45 +0900

    로그인 기능 수정

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-import "../css/login.css";
+import "../css/sign.css";
 
 const template = () => {
   const login = `
@@ -7,7 +7,7 @@ const template = () => {
                   <h1 class="w-[238px] h-[74px] indent-[-9999px] bg-[url('/images/Logo-hodu.png')] bg-no-repeat bg-contain">호두 오픈마켓</h1>
               </a>
           </header>
-          <section class="login-wrap max-w-[550px] w-full m-auto">
+          <form class="user__form max-w-[550px] w-full m-auto ">
               <h2 class="hidden">로그인 폼</h2>
               <ul class="user__btn__list">
                 <li>
@@ -17,34 +17,36 @@ const template = () => {
                     <button type="button" class="seller">판매회원 로그인</button>
                 </li>
               </ul>
-              <form class="customer">
+              <section class="user__section customer mb-[30px]">
                   <label for="userId" class="hidden">아이디 입력</label>
-                  <input type="text" placeholder="아이디" id="userId" required />
+                  <input type="text" class="login__input" placeholder="아이디" id="userId" required />
                   <label for="userPassword" class="hidden">비밀번호 입력</label>
-                  <input type="password" placeholder="비밀번호" id="userPassword" required />
-                  <button type="submit">로그인</button>
-              </form>
-              <div class="sign__up">
-                <a href="#sign-up">회원가입</a>
-                <a href="#login">비밀번호 찾기</a>
-              </div>
-          </section>
+                  <input type="password" class="login__input"  placeholder="비밀번호" id="userPassword" required />
+                  <button class="btn btn__green mt-[18px]" type="submit">로그인</button>
+              </section>
+            </form>
+            <div class="sign__up">
+              <a href="#sign-up">회원가입</a>
+              <a href="#login">비밀번호 찾기</a>
+            </div>
       `;
 
   return login;
 };
 
 /**
- * 제목2 로그인 폼 하위 태그 css 는 login.css로 처리
+ * 제목2 로그인 폼 하위 태그 css 는 대부분 sign.css로 처리
+ * class에 보여지는 tailwind 외에는 모두 css 파일로 관리됨
  */
 export const Login = () => {
   document.body.innerHTML = template();
 
   const url = "https://openmarket.weniv.co.kr";
   let loginUser = "BUYER";
-  const customerBtn = document.querySelector(".customer");
-  const sellerBtn = document.querySelector(".seller");
-  const form = document.querySelector(".login-wrap form");
+  const customerBtn = document.querySelector("button.customer");
+  const sellerBtn = document.querySelector("button.seller");
+  const form = document.querySelector(".user__form");
+  const section = document.querySelector(".user__section");
   const userId = document.getElementById("userId");
   const userPassword = document.getElementById("userPassword");
   let errorMessage = null;
@@ -53,10 +55,8 @@ export const Login = () => {
     unclickUserInfo.classList.remove("active");
     !clickUserInfo.classList.contains("active") &&
       clickUserInfo.classList.add("active");
-    if (form.classList.contains("seller")) {
-      form.classList.remove("seller");
-      form.classList.add("customer");
-    }
+      section.classList.remove(unclickUserInfo.classList);
+      section.classList.add(clickUserInfo.classList);
     loginUser = infoText;
     userId.value = "";
     userPassword = "";
