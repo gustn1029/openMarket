05a9fa63055212a5f8d6c63c19a7df9f4cf99c4e
hs0820 <gustn1029@gmail.com>
2024-08-05 17:54:26 +0900

    헤더 호버시 마이페이지 서브메뉴 노출

--- js/header.js
+++ js/header.js
@@ -1,11 +1,22 @@
 export const Header = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   let listArr = [];
+  const myPageChildren = [
+    {
+      href: '#my-page',
+      text: "마이페이지",
+    },
+    {
+      href: "#logout",
+      text: "로그아웃",
+    },
+  ];
 
   const dependentItem = {
-    href: user ? "#my-page" : "#login",
+    href: user ? `#` : "#login",
     text: user ? "마이페이지" : "로그인",
     imageUrl: "/images/icon-user.svg",
+    children: user ? myPageChildren:[],
   };
 
   const buyerListItem = [
@@ -27,7 +38,7 @@ export const Header = () => {
   buyerListItem.push(dependentItem);
   sellerListItem.unshift(dependentItem);
 
-  if(user && user.user_type === "SELLER") {
+  if (user && user.user_type === "SELLER") {
     listArr = sellerListItem;
   } else {
     listArr = buyerListItem;
@@ -40,28 +51,39 @@ export const Header = () => {
                     <h1 class="w-[124px] h-[38px] indent-[-9999px] bg-[url('/images/Logo-hodu.png')] bg-no-repeat bg-cover">호두 오픈마켓</h1>
                 </a>
                 <label for="search-input" class="max-w-[400px] w-full flex items-center px-[22px] py-[9px] rounded-[50px] border-[2px] border-[#21BF48]">
-                <span class="hidden">검색어 입력</span>
+                <span class="tag__hidden">검색어 입력</span>
                     <input type="text" placeholder="상품을 검색해보세요!" class="leading-[20px] flex-1 outline-none" id="search-input" />
                     <button type="button" class="w-[28px] h-[28px] bg-[url('/images/icon-search.svg')] bg-no-repeat bg-cover indent-[-9999px]">검색</button>
                 </label>
             </div>
             <nav>
-            <ul class="flex items-center">
-                ${
-                  listArr.map((el) => {
-                          let itemPublish = "";
-                          if (el.text === "판매자 센터") {
-                            itemPublish = `<a href="${el.href}" class="block text-left py-[18px] pl-[60px] ml-[30px] w-[168px] leading-[1.125rem] text-[1.125rem] text-white rounded-[5px]" style="background: url('/openMarket${el.imageUrl}') no-repeat 20px center; background-color: #21BF48;">${el.text}</a>`
-                          } else {
-                            itemPublish = `<a href="${el.href}" class="block w-[64px] text-center leading-[0.875rem] text-[0.75rem] text-[#767676] pt-[36px]" style="background: url('/openMarket${el.imageUrl}') no-repeat center top">${el.text}</a>`;
-                          }
-                          return `
-                              <li class="">
+            <ul class="flex items-center nav__list">
+                ${listArr
+                  .map((el) => {
+                    let itemPublish = "";
+                    if (el.text === "판매자 센터") {
+                      itemPublish = `<a href="${el.href}" class="block text-left py-[18px] pl-[60px] ml-[30px] w-[168px] leading-[1.125rem] text-[1.125rem] text-white rounded-[5px]" style="background: url('/openMarket${el.imageUrl}') no-repeat 20px center; background-color: #21BF48;">${el.text}</a>`;
+                    } else {
+                      itemPublish = `<a href="${el.href}" class="block w-[64px] text-center leading-[0.875rem] text-[0.75rem] text-[#767676] pt-[36px]" style="background: url('/openMarket${el.imageUrl}') no-repeat center top">${el.text}</a>`;
+                    }
+
+                    if(el.children && el.children.length > 0) {
+                      itemPublish += `
+                        <ul class="absolute hidden group-hover/item:grid gap-[8px] w-[130px] p-[10px] bg-white rounded-[5px] drop-shadow-[0_0_6px_rgba(0,0,0,0.25)] left-[50%] top-[calc(100%+14px)] translate-x-[-50%]">
+                          ${el.children.map((item) => {
+                              return `<li class="relative z-[1]">
+                                <a href="${item.href}" class="block text-center text-[#767676] w-full py-[10px] border rounded-[5px] border-white hover:border-[#767676] hover:text-black">${item.text}</a>
+                              </li>`
+                            }).join("")}
+                        </ul>
+                      `
+                    }
+                    return `
+                              <li class="relative group/item">
                                 ${itemPublish}
                               </li>`;
-                        })
-                        .join("")
-                }
+                  })
+                  .join("")}
                 </ul>
             </nav>
         </section>
