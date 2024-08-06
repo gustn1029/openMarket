e78619e49c8d96c7d328ecc010fc773391bf0030
hs0820 <gustn1029@gmail.com>
2024-08-06 19:49:29 +0900

    헤더 기능 수정, 헤더 css 수정

--- js/header.js
+++ js/header.js
@@ -1,10 +1,11 @@
-export const Header = () => {
+const template = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   let listArr = [];
   const myPageChildren = [
     {
-      href: '#my-page',
+      href: "#my-page",
       text: "마이페이지",
+      class: "myPage",
     },
     {
       href: "#logout",
@@ -15,15 +16,15 @@ export const Header = () => {
   const dependentItem = {
     href: user ? `#` : "#login",
     text: user ? "마이페이지" : "로그인",
-    imageUrl: "/images/icon-user.svg",
-    children: user ? myPageChildren:[],
+    class: user ? `myPage` : "login",
+    children: user ? myPageChildren : [],
   };
 
   const buyerListItem = [
     {
       href: "#cart",
       text: "장바구니",
-      imageUrl: "/images/icon-shopping-cart.svg",
+      class: "cart",
     },
   ];
 
@@ -31,7 +32,6 @@ export const Header = () => {
     {
       href: "#seller-center",
       text: "판매자 센터",
-      imageUrl: "/images/icon-shopping-bag.svg",
     },
   ];
 
@@ -44,7 +44,6 @@ export const Header = () => {
     listArr = buyerListItem;
   }
   const $header = `
-    <header class="">
         <section class="inner flex w-full bg-white py-[20px]">
             <div class="flex items-center grow gap-[30px]">
                 <a href="/openMarket">
@@ -62,24 +61,27 @@ export const Header = () => {
                   .map((el) => {
                     let itemPublish = "";
                     if (el.text === "판매자 센터") {
-                      itemPublish = `<a href="${el.href}" class="block text-left py-[18px] pl-[60px] ml-[30px] w-[168px] leading-[1.125rem] text-[1.125rem] text-white rounded-[5px]" style="background: url('/openMarket${el.imageUrl}') no-repeat 20px center; background-color: #21BF48;">${el.text}</a>`;
+                      itemPublish = `<a href="${el.href}" class="seller block text-left py-[18px] pl-[60px] ml-[30px] w-[168px] leading-[1.125rem] text-[1.125rem] text-white rounded-[5px] bg-[#21BF48] bg-no-repeat bg-top">${el.text}</a>`;
                     } else {
-                      itemPublish = `<a href="${el.href}" class="block w-[64px] text-center leading-[0.875rem] text-[0.75rem] text-[#767676] pt-[36px]" style="background: url('/openMarket${el.imageUrl}') no-repeat center top">${el.text}</a>`;
+                      // bg 이미지는 hover 처리를 위해 style.css로 처리
+                      itemPublish = `<a href="${el.href}" class="nav__list__item ${el.class} block w-[64px] text-center leading-[0.875rem] text-[0.75rem] text-[#767676] pt-[36px]">${el.text}</a>`;
                     }
 
-                    if(el.children && el.children.length > 0) {
+                    if (el.children && el.children.length > 0) {
                       itemPublish += `
                         <ul class="absolute hidden group-hover/item:grid gap-[8px] w-[130px] p-[10px] bg-white rounded-[5px] drop-shadow-[0_0_6px_rgba(0,0,0,0.25)] left-[50%] top-[calc(100%+14px)] translate-x-[-50%]">
-                          ${el.children.map((item) => {
+                          ${el.children
+                            .map((item) => {
                               return `<li class="relative z-[1]">
                                 <a href="${item.href}" class="block text-center text-[#767676] w-full py-[10px] border rounded-[5px] border-white hover:border-[#767676] hover:text-black">${item.text}</a>
-                              </li>`
-                            }).join("")}
+                              </li>`;
+                            })
+                            .join("")}
                         </ul>
-                      `
+                      `;
                     }
                     return `
-                              <li class="relative group/item">
+                              <li class="relative nav__list__item bg-no-repeat bg-top group/item">
                                 ${itemPublish}
                               </li>`;
                   })
@@ -87,8 +89,21 @@ export const Header = () => {
                 </ul>
             </nav>
         </section>
-    </header>
     `;
 
   return $header;
+};
+
+export const Header = () => {
+  const header = document.createElement("header");
+  const headerTemp = template();
+  header.id = "header";
+  header.insertAdjacentHTML("beforeend", headerTemp);
+
+  const myPage = header.querySelector(".myPage");
+  if (myPage) {
+    myPage.addEventListener("click", (e) => e.preventDefault());
+  }
+
+  return header;
 };
