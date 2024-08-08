1947802b87c5ae8ac570bc2fddad6be99b907f46
hs0820 <gustn1029@gmail.com>
2024-08-08 18:40:58 +0900

    로그아웃 기능 수정

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,9 @@
 const Logout = () => {
     localStorage.removeItem("user");
-    window.location.href = "/openMarket/";
+    const beforeHash = localStorage.getItem("beforePage");
+    window.location.href = `/openMarket/${beforeHash ? beforeHash: ""}`;
+    window.location.reload();
+    localStorage.removeItem("beforePage");
 };
 
 export default Logout;
