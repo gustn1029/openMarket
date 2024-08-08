76987e2bf5d72573c98647726f36f678655ecaf9
hs0820 <gustn1029@gmail.com>
2024-08-08 18:41:26 +0900

    홈 기능 수정

--- js/components/Home.js
+++ js/components/Home.js
@@ -1,21 +1,31 @@
+// components/Home.js
 import { Header } from "../header";
-import { root } from "../main";
 import ProductList from "../productList.js";
 import Details from "../details.js";
+import Cart from "../cart.js";
+import Loading from "./loading/Loading.js";
+import { root } from "../main.js";
 
-const Home = async (content="",id="") => {
-    const $main = document.createElement("main");
-    $main.classList.add("m-[80px_0_180px]")
-    if(content ==="") {
-        $main.appendChild(await ProductList());
-    } else if (content ==="details") {
-        $main.appendChild(await Details(id));
-    }
-    root.innerHTML = "";
-    root.appendChild(Header());
-    root.appendChild($main);
+const Home = async (content = "", id = "") => {
+  const $main = document.createElement("main");
+  const loading = Loading();
+  root.appendChild(loading);
 
-    return root;
-}
+  $main.classList.add("m-[80px_0_180px]");
+  if (content === "") {
+    $main.appendChild(await ProductList()); // 기본 페이지 로드
+  } else if (content === "details") {
+    $main.appendChild(await Details(id)); // 상세 페이지 로드
+  } else if (content === "cart") {
+    $main.appendChild(await Cart()); // 장바구니 페이지 로드
+  }
+
+  root.innerHTML = "";
+  root.appendChild(Header());
+  root.appendChild($main);
+  loading.remove();
+
+  return root;
+};
 
 export default Home;
