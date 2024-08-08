1a2dbc04a05ef2b40306056482cf42e51de20754
hs0820 <gustn1029@gmail.com>
2024-08-08 18:41:14 +0900

    로딩 컴포넌트 생성

---
+++ js/components/loading/Loading.js
@@ -0,0 +1,10 @@
+import "./loading.css";
+
+const Loading = () => {
+  const loading = document.createElement("div");
+  loading.classList.add("loading");
+
+  return loading;
+};
+
+export default Loading;
