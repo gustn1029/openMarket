f61fc0a00f640ee623016762136c41bdecc1ef34
hs0820 <gustn1029@gmail.com>
2024-08-10 23:55:02 +0900

    라벨인풋 컴포넌트 수정

--- js/components/LabelInput.js
+++ js/components/LabelInput.js
@@ -1,8 +1,8 @@
-const LabelInput = (id, labelText, type = "text",inputClass="", labelClass = "") => {
+const LabelInput = (id, labelText, type = "text",inputClass="", labelClass = "", required = true) => {
     return `
       <label for="${id}" class="${labelClass} relative">
           <span>${labelText}</span>
-          <input class="${inputClass}" type="${type}" id="${id}" required />
+          <input class="${inputClass}" type="${type}" id="${id}" ${required ? "required":""} />
       </label>
     `;
   };
