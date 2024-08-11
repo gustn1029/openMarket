976211cc3be1e42518694e5f2c22eb8eb40f47d9
hs0820 <gustn1029@gmail.com>
2024-08-11 17:02:01 +0900

    배너 추가

--- js/components/Home.js
+++ js/components/Home.js
@@ -7,6 +7,7 @@ import Loading from "./loading/Loading.js";
 import { root } from "../main.js";
 import Order from "../order.js";
 import Footer from "../footer.js";
+import Banner from "./banner/Banner.js";
 
 /**
  * 
@@ -15,12 +16,30 @@ import Footer from "../footer.js";
  * @returns 
  */
 const Home = async (content = "", id = "") => {
+  const bannerData = [
+    {
+      imageUrl:"https://images.unsplash.com/photo-1626544827763-d516dce335e2?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link:"/openMarket/#",
+      text:"호두샵 배너 이미지1",
+    },
+    {
+      imageUrl:"https://images.unsplash.com/photo-1622547748225-3fc4abd2cca0?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link:"",
+      text:"호두샵 배너 이미지2",
+    },
+    {
+      imageUrl:"https://images.unsplash.com/photo-1653152748678-6d843daabac5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      link:"",
+      text:"호두샵 배너 이미지3",
+    }
+  ]
   const $main = document.createElement("main");
   const loading = Loading();
+  const banner = Banner(bannerData, false, "max-h-[500px] h-full overflow-hidden");
   root.appendChild(loading);
 
-  $main.classList.add("m-[80px_0_180px]");
   if (content === "") {
+    $main.appendChild(banner);
     $main.appendChild(await ProductList()); // 기본 페이지 로드
   } else if (content === "details") {
     $main.appendChild(await Details(id)); // 상세 페이지 로드
@@ -28,6 +47,13 @@ const Home = async (content = "", id = "") => {
     $main.appendChild(await Cart()); // 장바구니 페이지 로드
   } else if (content === "order") {
     $main.appendChild(await Order()); // 장바구니 페이지 로드
+  }
+
+  if(content !== "") {
+    $main.classList.add("m-[80px_0_180px]");
+    banner && banner.remove();
+  } else {
+    $main.classList.add("m-[0_0_180px]");
   }
 
   root.innerHTML = "";
---
+++ js/components/banner/Banner.js
@@ -0,0 +1,77 @@
+import "./banner.css";
+
+const template = (data, isLink, sliderClass, idx = 0) => {
+  const banner = `
+        <h2 class="tag__hidden">호두샵 메인페이지 배너</h2>
+        <div class="slider relative ${sliderClass}">
+            ${data.map((el, i) => {
+              return `
+                    <div class="slide absolute lef-0 top-0 ${
+                      i === idx ? "opacity-100" : "opacity-0"
+                    } transition-all duration-300">
+                        ${isLink ? `<a href="${data[idx].link}">` : ""}
+                            <img class="" src="${el.imageUrl}" alt="${
+                el.text
+              }" />
+                        ${isLink ? `</a>` : ""}
+                    </div>
+                `;
+            }).join("")}
+            <button class="slider__prev__btn">이전</button>
+            <button class="slider__next__btn">다음</button>
+        </div>
+    `;
+
+  return banner;
+};
+
+/**
+ *
+ * @param data 배너에 사용할 이미지 데이터(배열)
+ * data = {
+ *      imageUrl: string,
+ *      link: string,
+ *      text: string
+ * }
+ * @param isLink 링크 이동 여부
+ * @param sliderClass 슬라이더에 추가할 클래스
+ */
+const Banner = (data, isLink = true, sliderClass = "") => {
+  let showIndex = 0;
+  const temp = template(data, isLink, sliderClass, showIndex);
+  const section = document.createElement("section");
+  section.classList.add("banner");
+  section.insertAdjacentHTML("beforeend", temp);
+
+
+  const slides = section.querySelectorAll(".slide");
+
+  const updateSlides = () => {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle("opacity-100", i === showIndex);
+      slide.classList.toggle("opacity-0", i !== showIndex);
+    });
+  };
+
+  const bannerBtnClickHandler = (btn) => {
+    if(btn.textContent === "이전") {
+        showIndex = showIndex > 0 ? showIndex - 1 : data.length - 1;
+    } else {
+        showIndex = showIndex >= data.length - 1 ? 0 : showIndex + 1;
+    }
+    updateSlides();
+  };
+
+  // Initial update
+  updateSlides();
+
+  section.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      bannerBtnClickHandler(e.target);
+    }
+  });
+
+  return section;
+};
+
+export default Banner;
