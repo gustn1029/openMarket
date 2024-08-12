34ac5132d28cea91e7c3b2a53bb09f2a36a1068d
hs0820 <gustn1029@gmail.com>
2024-08-12 09:26:11 +0900

    슬라이드 z인덱스 수정

--- js/components/banner/Banner.js
+++ js/components/banner/Banner.js
@@ -9,7 +9,7 @@ const template = (data, isLink, sliderClass, idx = 0) => {
                     <div class="slide absolute lef-0 top-0 ${
                       i === idx ? "opacity-100" : "opacity-0"
                     } transition-all duration-300">
-                        ${isLink ? `<a href="${data[idx].link}">` : ""}
+                        ${isLink ? `<a href="${el.link}">` : ""}
                             <img class="" src="${el.imageUrl}" alt="${
                 el.text
               }" />
@@ -49,7 +49,9 @@ const Banner = (data, isLink = true, sliderClass = "") => {
   const updateSlides = () => {
     slides.forEach((slide, i) => {
       slide.classList.toggle("opacity-100", i === showIndex);
+      slide.classList.toggle("z-[1]", i === showIndex);
       slide.classList.toggle("opacity-0", i !== showIndex);
+      slide.classList.toggle("", i !== showIndex);
     });
   };
 
