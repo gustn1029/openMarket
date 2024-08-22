6d81927f7e3def4654ad01bb9bd60c73ce7c519b
hs0820 <gustn1029@gmail.com>
2024-08-22 17:51:10 +0900

    판매자 판매상품리스트 작업

--- js/components/seller/SellerProductList.js
+++ js/components/seller/SellerProductList.js
@@ -1,22 +1,43 @@
-const thData = ["상품 정보", "판매 가격", "수정", "삭제"]
+const fetchData = async (page = "") => {
+  try {
+    const res = await fetch(`${url}/seller/${page && `?page=${page}`}`);
+    if (res.ok) {
+      const json = await res.json();
+      return json;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const template = () => {
+const tbodyTemplate = (page) =>  {
+  const data = fetchData(page);
+}
+
+const thData = ["상품 정보", "판매 가격", "수정", "삭제"];
+
+const template = (page) => {
   const productList = `
     <h3 class="tag__hidden">상품 리스트</h3>
-    <table class="w-full rounded-[5px] overflow-hidden">
+    <table class="w-full border-collapse">
       <colgroup>
         <col class="w-[50%]" />
         <col class="w-[calc(50%-280px)]" />
         <col class="w-[140px]" />
         <col class="w-[140px]" />
       </colgroup>
-      <thead>
+      <thead class="border-b border-b-[#c4c4c4]">
         <tr>
-          ${thData.map((el) => `<th class="py-[20px] leading-[20px] text-[1.125rem] bg-white">상품 정보</th>`).join("")}
+          ${thData
+            .map(
+              (el) =>
+                `<th class="py-[20px] leading-[20px] text-[1.125rem] bg-white">상품 정보</th>`
+            )
+            .join("")}
         </tr>
       </thead>
     </table>
-  `
+  `;
   return productList;
 };
 
@@ -27,6 +48,7 @@ const SellerProductList = () => {
   section.classList.add("border");
   section.classList.add("border-[#c4c4c4]");
   section.classList.add("rounded-[5px]");
+  section.classList.add("overflow-hidden");
   const temp = template();
 
   section.insertAdjacentHTML("beforeend", temp);
