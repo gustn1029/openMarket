d9051e7ef6ec210d00f1804b6d212d9552f8218a
hs0820 <gustn1029@gmail.com>
2024-08-27 17:55:53 +0900

    판매자 센터 타이틀 부분 수정

--- js/components/seller/SellerCenterTitle.js
+++ js/components/seller/SellerCenterTitle.js
@@ -1,12 +1,12 @@
-import "./sellerCenterTitle.css"
+import "./sellerCenterTitle.css";
 
-const SellerCenterTitle = (title = "", hash = "") => {
+const template = (title = "", hash = "") => {
   let template = "";
   if (hash === "make-product") {
     template = `<h2 class="col-span-2 py-[42px]">상품 등록</h2>`;
   } else {
     template = `
-            <div class="col-span-2 w-full py-[42px] flex justify-between items-center">
+            <div class="w-full py-[42px] flex justify-between items-center">
                 <div class="text-[2.25rem] flex items-center">
                     <h2 class="sans-bold">대시보드</h2>
                     <strong class="font-[400] ml-[16px] text-[#21BF48]">${title}</strong>
@@ -17,6 +17,15 @@ const SellerCenterTitle = (title = "", hash = "") => {
   }
 
   return template;
+};
+
+const SellerCenterTitle = (title = "", hash = "") => {
+  const div = document.createElement("div");
+  const temp = template(title, hash);
+  div.insertAdjacentHTML("beforeend", temp);
+  div.classList.add("col-span-2")
+
+  return div;
 };
 
 export default SellerCenterTitle;
