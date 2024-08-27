82acea2f31f676dba1b6e2d079583975a131285b
hs0820 <gustn1029@gmail.com>
2024-08-27 17:55:44 +0900

    판매자 센터 템플릿 수정

--- js/components/seller/SellerTemplate.js
+++ js/components/seller/SellerTemplate.js
@@ -2,12 +2,12 @@ import SellerCenterTitle from "./SellerCenterTitle";
 import SellerNav from "./SellerNav";
 import "./sellerTemplate.css";
 
+
 const SellerTemplate = (childTemp, title) => {
   const hash = window.location.hash.slice(1);
   const sellerTemp = document.createElement("div");
   sellerTemp.classList.add("seller__template");
-  sellerTemp.insertAdjacentHTML("beforeend", SellerCenterTitle(title)
-  );
+  sellerTemp.appendChild(SellerCenterTitle(title));
   sellerTemp.appendChild(SellerNav(hash));
   sellerTemp.appendChild(childTemp);
   return sellerTemp;
